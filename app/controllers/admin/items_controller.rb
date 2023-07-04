a8e61386201238976c2da7f77b2a3f895c4a65b3
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-07-04 04:51:30 +0000

    [add] admin-item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,39 @@
 class Admin::ItemsController < ApplicationController
-  
+
+  def index
+    @items = Item.all
+  end
+
   def new
     @item = Item.new
+    @genres = Genre.pluck(:name, :id)
   end
-  
+
   def create
     item = Item.new(item_params)
     item.save
     redirect_to admin_items_path
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.pluck(:name, :id)
+  end
+  
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_available)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,24 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
   has_many :carts
   has_many :order_details
-  belongs_to :genres
-  
+  belongs_to :genre
+
   validates :genre_id,     presence: true
   validates :name,         presence: true
   validates :introduction, presence: true
   validates :price,        presence: true
-  
+
   def item_status
-    if is available == true
+    if is_available == true
       "販売中"
     else
       "販売停止中"
     end
   end
-  
+
   def with_tax_price
-    ( price * 1.1).florr
+    (price * 1.1).floor
   end
 end
